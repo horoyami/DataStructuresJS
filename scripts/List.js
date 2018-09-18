19a26ae7e9b82fcc2beacf4e727ff901ebefcda0
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-09-18 11:44:14 +0300

    Mod for List

--- scripts/List.js
+++ scripts/List.js
@@ -1,13 +1,16 @@
 'use strict';
 
-// TODO замыкание и итерирование
-
 class List {
 
     constructor(arr) {
         this._root = null;
         this._last = null;
         this.size = 0;
+
+        if (arr instanceof Array) {
+            for (let i = 0; i < arr.length; i++)
+                this.back = arr[i];
+        }
 
         this[Symbol.iterator] = function () {
 
@@ -16,7 +19,7 @@ class List {
             return {
                 next() {
                     if (current !== null) {
-                        let x = current.value;
+                        let x = current;
                         current = current.right;
                         return {
                             done: false,
@@ -30,6 +33,44 @@ class List {
                 }
             };
         };
+    }
+
+    insert(node, value) {
+        if (node !== null) {
+            let left = node;
+            let right = node.right;
+            let newNode = List._createNode(value);
+            this.size++;
+            left.right = newNode;
+            newNode.left = left;
+            if (right !== null) {
+                right.left = newNode;
+                newNode.right = right;
+            } else {
+                this._last = newNode;
+            }
+        }
+    }
+
+    remove(node) {
+        if (node == null)
+            return;
+        let left = node.left;
+        let right = node.right;
+        this.size--;
+        if (left !== null) {
+            left.right = right;
+            node.left = null;
+        }
+        if (right !== null) {
+            right.left = left;
+            node.right = null;
+        }
+        if (node === this._last)
+            this._last = left;
+        if (node === this._root)
+            this._root = right;
+
     }
 
     isEmpty() {
@@ -67,17 +108,17 @@ class List {
     }
 
     get front() {
-        return List._getNodeValue(this._root);
+        return this._root;
     }
 
     get back() {
-        return List._getNodeValue(this._last);
+        return this._last;
     }
 
     _add(elem, where) {
         let newNode = List._createNode(elem);
         this.size++;
-        if (this._root == null) {
+        if (this._root === null) {
             this._root = newNode;
             this._last = newNode;
         } else {
@@ -94,13 +135,6 @@ class List {
             where(this);
             this.size--;
         }
-    }
-
-    static _getNodeValue(node) {
-        if (node === null)
-            return undefined;
-        else
-            return node.value;
     }
 
     static _createNode(value = undefined) {
--- tests/List.js
+++ tests/List.js
@@ -10,22 +10,22 @@ describe("List test", function () {
 
     it("Created List should be empty", function () {
         let list = new List();
-        chai.assert.equal(list.front, undefined);
-        chai.assert.equal(list.back, undefined);
+        chai.assert.equal(list.front, null);
+        chai.assert.equal(list.back, null);
     });
 
     it("Push first element by back", function () {
         let list = new List();
         list.back = 5;
-        chai.assert.equal(list.front, 5);
-        chai.assert.equal(list.back, 5);
+        chai.assert.equal(list.front.value, 5);
+        chai.assert.equal(list.back.value, 5);
     });
 
     it("Push first element by front", function () {
         let list = new List();
         list.front = 5;
-        chai.assert.equal(list.front, 5);
-        chai.assert.equal(list.back, 5);
+        chai.assert.equal(list.front.value, 5);
+        chai.assert.equal(list.back.value, 5);
     });
 
     it("Push back many elems", function () {
@@ -35,8 +35,8 @@ describe("List test", function () {
         list.back = 3;
         list.back = 4;
         list.back = 5;
-        chai.assert.equal(list.front, 1);
-        chai.assert.equal(list.back, 5);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.back.value, 5);
     });
 
     it("Push front many elems", function () {
@@ -46,8 +46,8 @@ describe("List test", function () {
         list.front = 3;
         list.front = 4;
         list.front = 5;
-        chai.assert.equal(list.front, 5);
-        chai.assert.equal(list.back, 1);
+        chai.assert.equal(list.front.value, 5);
+        chai.assert.equal(list.back.value, 1);
     });
 
     it("Size", function () {
@@ -67,8 +67,8 @@ describe("List test", function () {
         list.front = 3;
         list.back = 4;
         list.front = 5;
-        chai.assert.equal(list.front, 5);
-        chai.assert.equal(list.back, 4);
+        chai.assert.equal(list.front.value, 5);
+        chai.assert.equal(list.back.value, 4);
     });
 
     it("PopBack", function () {
@@ -80,7 +80,7 @@ describe("List test", function () {
         list.front = 1;
         list.popBack();
         list.popBack();
-        chai.assert.equal(list.back, 3);
+        chai.assert.equal(list.back.value, 3);
     });
 
     it("PopFront", function () {
@@ -92,7 +92,7 @@ describe("List test", function () {
         list.back = 1;
         list.popFront();
         list.popFront();
-        chai.assert.equal(list.front, 3);
+        chai.assert.equal(list.front.value, 3);
     });
 
     it("1 element after many ops", function () {
@@ -123,8 +123,8 @@ describe("List test", function () {
         list.popBack();
         list.popBack();
         chai.assert.equal(list.size, 0);
-        chai.assert.equal(list.back, undefined);
-        chai.assert.equal(list.back, undefined);
+        chai.assert.equal(list.back, null);
+        chai.assert.equal(list.back, null);
     });
 
     it("More pop than size", function () {
@@ -134,8 +134,8 @@ describe("List test", function () {
         list.popBack();
         list.popBack();
         chai.assert.equal(list.size, 0);
-        chai.assert.equal(list.back, undefined);
-        chai.assert.equal(list.back, undefined);
+        chai.assert.equal(list.back, null);
+        chai.assert.equal(list.back, null);
     });
 
     it("IsEmpty", function () {
@@ -157,7 +157,7 @@ describe("List test", function () {
         let mas = [];
 
         for (let v of list) {
-            mas.push(v);
+            mas.push(v.value);
         }
 
         chai.assert.equal(mas.length, 5);
@@ -177,12 +177,12 @@ describe("List test", function () {
         let mas = [];
 
         for (let v of list) {
-            let x = v;
+            let x = v.value;
             x = x + x;
         }
 
         for (let v of list) {
-            mas.push(v);
+            mas.push(v.value);
         }
 
         chai.assert.equal(mas.length, 5);
@@ -192,4 +192,90 @@ describe("List test", function () {
         }
 
     });
+
+    it("Create list by Array", function () {
+        let list = new List([1, 2, 3]);
+        chai.assert.equal(list.size, 3);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.back.value, 3);
+    });
+
+    it("0 is not special", function () {
+        let list = new List([0, 0, 0]);
+        let mas = [];
+
+        for (let v of list) {
+            mas.push(v.value);
+        }
+
+        chai.assert.equal(mas.length, 3);
+
+        for (let i = 0; i < mas.length; i++) {
+            chai.assert.equal(mas[i], 0);
+        }
+    });
+
+    it("insert in center", function () {
+        let list = new List([1, 2]);
+        let left = list.front;
+        list.insert(left, 5);
+        chai.assert.equal(list.size, 3);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.front.right.value, 5);
+        chai.assert.equal(list.front.right.right.value, 2);
+        chai.assert.equal(list.front.right.right.right, null);
+    });
+
+    it("insert in end", function () {
+        let list = new List([1, 2]);
+        let left = list.back;
+        list.insert(left, 5);
+        chai.assert.equal(list.size, 3);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.front.right.value, 2);
+        chai.assert.equal(list.front.right.right.value, 5);
+        chai.assert.equal(list.front.right.right.right, null);
+    });
+
+    it("remove from center", function () {
+        let list = new List([1, 2, 3]);
+        let node = list.front.right;
+        list.remove(node);
+        chai.assert.equal(list.size, 2);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.front.right.value, 3);
+        chai.assert.equal(list.front.right.right, null);
+    });
+
+    it("remove from end", function () {
+        let list = new List([1, 2, 3]);
+        let node = list.front.right.right;
+        list.remove(node);
+        chai.assert.equal(list.size, 2);
+        chai.assert.equal(list.front.value, 1);
+        chai.assert.equal(list.front.right.value, 2);
+        chai.assert.equal(list.front.right.right, null);
+        chai.assert.equal(list.back.value, 2);
+    });
+
+    it("remove from start", function () {
+        let list = new List([1, 2, 3]);
+        let node = list.front;
+        list.remove(node);
+        chai.assert.equal(list.size, 2);
+        chai.assert.equal(list.front.value, 2);
+        chai.assert.equal(list.front.right.value, 3);
+        chai.assert.equal(list.front.right.right, null);
+    });
+
+    it("remove all", function () {
+        let list = new List([1, 2, 3]);
+        list.remove(list.front);
+        list.remove(list.back);
+        list.remove(list.front);
+        chai.assert.equal(list.front, null);
+        chai.assert.equal(list.back, null);
+        chai.assert.equal(list.size, 0);
+    });
+
 });
